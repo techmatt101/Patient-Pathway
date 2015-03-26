8bd22dad0d166606f0ceadf6266a586ef6a426a1
techmatt101 <matt@kempmail.co.uk>
2015-03-26 01:31:14 +0000

    loading bar now includes loading in views and controllers

--- app/app.ts
+++ app/app.ts
@@ -5,11 +5,24 @@ import RoutingUtil = require('./utils/routing-util');
 
 var PatientPathway = angular.module('PatientPathway', [
     'ngRoute',
-    'ngAnimate',
+    'ngAnimate',  //TODO: hopefully remove this soon as it's only needed for the loading bar - https://github.com/chieffancypants/angular-loading-bar/issues/33
     'angular-loading-bar',
     'PatientPathway.User',
     'PatientPathway.Pathway'
 ])
+    .run(($rootScope, cfpLoadingBar) => {
+        var timeout;
+        $rootScope.$on('$routeChangeStart', function() {
+            timeout = setTimeout(() => {
+                cfpLoadingBar.start();
+                cfpLoadingBar.inc();
+            }, 100);
+        });
+        $rootScope.$on('$routeChangeSuccess', function() {
+            clearTimeout(timeout);
+            cfpLoadingBar.complete();
+        });
+    })
     .controller('Navbar', RoutingUtil.legacyAsyncLoadController('components/navbar/navbar.controller'))
     .config(($routeProvider : ng.route.IRouteProvider, cfpLoadingBarProvider) => {
         cfpLoadingBarProvider.includeSpinner = false;
