854178912d3d1e37274ee83cfdcbb6dff5003c39
techmatt101 <matt@kempmail.co.uk>
2015-04-10 21:00:22 +0100

    backend refactor to support the new integration

--- app/services/backend-connection-service.ts
+++ app/services/backend-connection-service.ts
@@ -18,27 +18,55 @@ class BackendConnectionService {
     private _request : ng.IHttpService;
 
     private _url = 'http://1d7699839159eb08ad51551d01fd6873.wp.patientpath.co.uk/';
-    private _token = '6b1adee9b2bd36fe115b70332ba50d9b7f5449e7';
+    private _token = '';
+    private _timestamp = 0;
 
 
-    constructor($q, $http) {
+    constructor ($q, $http) {
         this._Promise = $q;
         this._request = $http;
     }
 
-    get(path, data? : Object) : ng.IPromise<any> {
+    private requestHandler (request : ng.IHttpPromise<any>) : ng.IPromise<any> {
         return new this._Promise((resolve, reject) => {
-            //this._request.post(this._url + path, {
-            //    "args": data,
-            //    "token": this._token,
-            //    "timestamp": Date.now()
-            //})
-            this._request.get('mock-data/' + path + '.json')
-                .success((data : IResponse) => {
-                    (data.success) ? resolve(data.response) : reject(data.error);
-                })
-                .error(reject);
+            request.success((data : IResponse) => {
+                if (data.success) {
+                    this._token = data.token;
+                    this._timestamp = data.timestamp;
+                    resolve(data.response);
+                } else {
+                    reject(data.error);
+                }
+            });
+            request.error(reject);
         });
+    }
+
+    private backendRequester (path, data? : Object) : ng.IPromise<any> {
+        return this.requestHandler(this._request.get('mock-data/' + path + '.json'));
+        //return this._request.post(this._url + path, {
+        //    "args": data,
+        //    "token": this._token,
+        //    "timestamp": this._timestamp
+        //});
+    }
+
+    get (path, data? : Object) : ng.IPromise<any> {
+        //if (new Date(this._timestamp).isBefore(new Date())) {
+        //    return new this._Promise((resolve, reject) => {
+        //        this.requestHandler(this._request.get(this._url + 'user/token'))
+        //            .then(() => this.backendRequester(path, data)
+        //                .then((data) => resolve(data))
+        //                .catch((data) => reject(data)))
+        //            .catch((data) => reject(data));
+        //    });
+        //}
+
+        return this.backendRequester(path, data);
+    }
+
+    getFromMockData (path, data? : Object) : ng.IPromise<any> {
+        return this.requestHandler(this._request.get('mock-data/' + path + '.json'));
     }
 }
 
